d2fd9d4e80a28f06f92491edadca1103405a42d4
Pradeep Kumar Singh <singhpradeepk@google.com>
2023-04-10 20:51:35 +0530

    Add CloudSQl service

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,31 +1,31 @@
-locals {
-  tf_sa = var.terraform_service_account
-}
+# locals {
+#   tf_sa = var.terraform_service_account
+# }
 
-provider "google" {
-  alias = "impersonate"
+# provider "google" {
+#   alias = "impersonate"
 
-  scopes = [
-    "https://www.googleapis.com/auth/cloud-platform",
-    "https://www.googleapis.com/auth/userinfo.email",
-  ]
-}
+#   scopes = [
+#     "https://www.googleapis.com/auth/cloud-platform",
+#     "https://www.googleapis.com/auth/userinfo.email",
+#   ]
+# }
 
 
-data "google_service_account_access_token" "default" {
-  provider               = google.impersonate
-  target_service_account = local.tf_sa
-  scopes                 = ["userinfo-email", "cloud-platform"]
-  lifetime               = "3600s"
-}
+# data "google_service_account_access_token" "default" {
+#   provider               = google.impersonate
+#   target_service_account = local.tf_sa
+#   scopes                 = ["userinfo-email", "cloud-platform"]
+#   lifetime               = "3600s"
+# }
 
-/******************************************
-  Provider credential configuration
- *****************************************/
-provider "google" {
-  access_token = data.google_service_account_access_token.default.access_token
-}
+# /******************************************
+#   Provider credential configuration
+#  *****************************************/
+# provider "google" {
+#   access_token = data.google_service_account_access_token.default.access_token
+# }
 
-provider "google-beta" {
-  access_token = data.google_service_account_access_token.default.access_token
-}
+# provider "google-beta" {
+#   access_token = data.google_service_account_access_token.default.access_token
+# }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,8 @@
 
-variable "terraform_service_account" {
-  description = "Service account email of the account to impersonate to run Terraform."
-  type        = string
-}
+# variable "terraform_service_account" {
+#   description = "Service account email of the account to impersonate to run Terraform."
+#   type        = string
+# }
 
 
 variable "project_id" {
